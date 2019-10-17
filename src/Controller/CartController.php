81e21956af57a0b6cb6e76d2016bbfc02a101d44
Samera Yahia <samera@debian>
2019-10-17 22:46:08 +0200

    réalisation du projet

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,16 +2,45 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\Cart\CartService;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class CartController extends AbstractController
 {
     /**
      * @Route("/panier", name="cart_index")
      */
-    public function index()
+    public function index(CartService $cartService)
+
     {
-        return $this->render('cart/index.html.twig', []);
+
+        return $this->render('cart/index.html.twig', [
+            'items' => $cartService->getFullCart(),
+            'total' => $cartService->getTotal()
+        ]);
+    }
+
+    /**
+     * @Route("panier/add/{id}", name="cart_add")
+     */
+    public function add($id, CartService $cartService)
+
+    {
+        $cartService->add($id);
+
+        return $this->redirectToRoute("cart_index");
+    }
+
+    /**
+     * @Route("/panier/remove/{id}", name="cart_remove")
+     */
+    public function remove($id, CartService $cartService)
+    {
+       
+        $cartService->remove($id);
+
+        return $this->redirectToRoute("cart_index");
     }
 }
